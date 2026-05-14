9ae62617e8acd41cc717a9e57825646e694ea1c8
abdallhqwe6-commits <abdallhqwe6@gmail.com>
2026-05-14 20:52:50 +0300

    Add files via upload

---
+++ indix.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCKMfu7vHr_u8zfvKtgyTgeMAN4KoFV9_Y",
+  authDomain: "abdallh-yrmoak.firebaseapp.com",
+  databaseURL: "https://abdallh-yrmoak-default-rtdb.firebaseio.com",
+  projectId: "abdallh-yrmoak",
+  storageBucket: "abdallh-yrmoak.firebasestorage.app",
+  messagingSenderId: "256279244269",
+  appId: "1:256279244269:web:da0f54af9ac6c9ecb80be4",
+  measurementId: "G-7FL5L97SZS"
+};
+
+const app = initializeApp(firebaseConfig);
